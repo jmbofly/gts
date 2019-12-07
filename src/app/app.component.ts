bfc94ee357d1860d21db27987e4874c134a78cd3
unknown <flynn-3199@cotc.edu>
2019-12-07 02:40:23 -0500

    created landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+
+// TODO: Add navigation/route functionality
+
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +12,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'gts';
 
+  constructor(private router: Router, private route: ActivatedRoute) { }
   videoLoaded(e: Event) {
     console.log('video loaded', e);
+  }
+
+  navigateTo(url: string) {
+    console.log('navigating to :', url)
+    return this.router.navigateByUrl(url)
   }
 }
