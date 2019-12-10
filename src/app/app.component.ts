6b97001823428e10c36d333baa72488887720baa
unknown <flynn-3199@cotc.edu>
2019-12-10 18:21:09 -0500

    landing modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 
-// TODO: Add navigation/route functionality
+// TODO: Add navigation/routes
+// TODO: Add email signup
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
-import { Component } from '@angular/core';
+
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,16 +15,50 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gts';
+export class AppComponent implements OnInit {
+  title = 'Global Technology Services, LLC';
+  showMenuBar = true;
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, public modal: NgbModal) {
+  }
+
+  ngOnInit() {
+  }
+
+  animateScroll(id) {
+    const anchor: HTMLElement = document.getElementById(id);
+    const top = anchor.offsetTop;
+    window.scrollTo({
+      top,
+      behavior: 'smooth',
+      left: 0
+    });
+    this.showMenuBar = false;
+    console.log('scrolled', { top, id, anchor });
+  }
+
+
+  checkNavWidth(el: any) {
+    return el.clientWidth > 760;
+  }
   videoLoaded(e: Event) {
     console.log('video loaded', e);
   }
 
-  navigateTo(url: string) {
+  navigateTo(url: string, external = false) {
     console.log('navigating to :', url)
+    if (external) {
+      window.location.href = url;
+      return;
+    }
     return this.router.navigateByUrl(url)
+  }
+
+  toggleMenuBar() {
+    this.showMenuBar = !this.showMenuBar;
+  }
+
+  signUpAndRedirect(email: string, extra?: { name: string, message: string }) {
+
   }
 }
